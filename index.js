121c220bee59cefa3fbefe5b30794c4bfe22c043
Aaditya <aadinema06@gmail.com>
2024-07-24 21:41:17 +0530

    once more

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+
+app.use(cors({
+  origin:["https://portfolio-frontend-swart-eta.vercel.app/"],
+  methods:["POST","GET"],
+  credentials:true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -43,7 +48,7 @@ app.get("/api/download-resume", (req, res) => {
 });
 
 // Serve static files from the React app
-app.use("/", express.static(path.join(__dirname, "public", "dist")));
+// app.use("/", express.static(path.join(__dirname, "public", "dist")));
 
 app.get("/api", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "dist/index.html"));
