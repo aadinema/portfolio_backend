1ba1068d3756d1fa641e78f307941e02f507611a
Aaditya <aadinema06@gmail.com>
2024-07-26 17:22:26 +0530

    render4

--- index.js
+++ index.js
@@ -27,7 +27,10 @@ const pool = new Pool({
 app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
   try {
-    const result = await pool.query("SELECT NOW()"); 
+    const result = await pool.query(
+      "INSERT INTO contacts (name, email, message) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, message]
+    );
     res.status(200).json(result.rows[0]);
   } catch (error) {
     console.error(error.message);
